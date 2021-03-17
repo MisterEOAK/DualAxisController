e499d5c4bc6abba298282f04b543d3dc9e1ec5f1
MisterEOAK <64244961+MisterEOAK@users.noreply.github.com>
2021-03-16 20:41:57 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+let yVal = 0
+let xVal =0
+basic.forever(function () {
+
+    xVal = pins.analogReadPin(AnalogPin.P0)
+    yVal = pins.analogReadPin(AnalogPin.P1)
+    if (xVal < 400) {
+        basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    } else if (xVal > 600) {
+        basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+   basic.clearScreen()
+
+    } else if (yVal > 600) {
+        basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+
+    } else if (yVal < 400) {
+        basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    }
+	basic.clearScreen()
+    
+})
