1fc613f5c806901711197fe3397d6c2d3c13cddd
MisterEOAK <64244961+MisterEOAK@users.noreply.github.com>
2021-03-17 13:58:11 -0800

    Added Button Push Funtionality

--- main.ts
+++ main.ts
@@ -1,45 +1,60 @@
+let xVal = 0
 let yVal = 0
-let xVal =0
 basic.forever(function () {
-
+    basic.clearScreen()
     xVal = pins.analogReadPin(AnalogPin.P0)
     yVal = pins.analogReadPin(AnalogPin.P1)
     if (xVal < 400) {
         basic.showLeds(`
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
     } else if (xVal > 600) {
         basic.showLeds(`
-        . . # . .
-        . . . # .
-        # # # # #
-        . . . # .
-        . . # . .
-        `)
-   basic.clearScreen()
-
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.clearScreen()
     } else if (yVal > 600) {
         basic.showLeds(`
-        . . # . .
-        . . # . .
-        # . # . #
-        . # # # .
-        . . # . .
-        `)
-
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
     } else if (yVal < 400) {
         basic.showLeds(`
-        . . # . .
-        . # # # .
-        # . # . #
-        . . # . .
-        . . # . .
-        `)
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.pause(200)
     }
-	basic.clearScreen()
-    
+
+    basic.clearScreen()
 })
